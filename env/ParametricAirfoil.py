a6cbcaa2e7d973c916be6b18f39ccf210e256eb8
Aditya Mali <adityamali2003@gmail.com>
2025-03-31 04:17:40 +0530

    Added Visualisation

--- env/ParametricAirfoil.py
+++ env/ParametricAirfoil.py
@@ -21,3 +21,33 @@ class ParametricAirfoil:
         # TODO: Implement the logic to modify the base points based on deltas provided by the model
 
         return modified_points
+    
+    def visualise_airfoil(self, points):
+        """
+        Visualise the airfoil shape based on the provided points.
+        :param points: List of points representing the airfoil shape.
+        """
+
+        x = [point[0] for point in points]
+        y = [point[1] for point in points]
+
+        plt.plot(x, y)
+        plt.title("Parametric Airfoil Shape")
+        plt.xlabel("X-axis")
+        plt.ylabel("Y-axis")
+        plt.axis('equal')
+        plt.grid()
+        plt.show()
+
+    def generate_base_points(self):
+        """
+        Generate the base points for the airfoil shape.
+        :return: List of base points.
+        """
+        base_points = []
+        for i in range(self.base_points):
+            x = i / (self.base_points - 1)
+            y = self.constraints['max_thickness'] * (0.2969 * np.sqrt(x) - 0.1260 * x - 0.3516 * x**2 + 0.2843 * x**3 - 0.1015 * x**4)
+            base_points.append((x, y))
+        return base_points
+        
